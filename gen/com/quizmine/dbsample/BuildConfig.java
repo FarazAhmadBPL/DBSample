b868e7361c3fa162242ddd30515a4091b227ccf0
faraz ahmad <ahmad.faraz764@gmail.com>
2014-05-30 20:13:56 +0530

    Change-Id: Idabec0fc6067595430f6d198c4a9dd2aece474ec Signed-off-by: faraz ahmad <ahmad.faraz764@gmail.com>

---
+++ gen/com/quizmine/dbsample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.quizmine.dbsample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/quizmine/dbsample/DataBaseWrapper.java
@@ -0,0 +1,37 @@
+package com.quizmine.dbsample;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseWrapper extends SQLiteOpenHelper{
+	public static final String STUDENTS = "Students";
+	public static final String STUDENT_ID = "_id";
+	public static final String STUDENT_NAME = "_name";
+
+	private static final String DATABASE_NAME = "Students.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	// creation SQLite statement
+	private static final String DATABASE_CREATE = "create table " + STUDENTS
+				+ "(" + STUDENT_ID + " integer primary key autoincrement, "
+				+ STUDENT_NAME + " text not null);";
+	
+	public DataBaseWrapper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(DATABASE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		db.execSQL("Drop table if exists "+STUDENTS);
+		onCreate(db);
+	}
+
+}
